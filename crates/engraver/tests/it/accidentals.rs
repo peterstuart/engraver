efaeaee59c5b17ffc4f0d5e3896bb104ed6e21d2
Alec Deason <alec@tinycountry.com>
2023-06-12 16:15:58 -0700

    Don't show accidentals on notes in the measure that follows a key signature alteration unless the note actually departs from the signature

--- crates/engraver/tests/it/accidentals.rs
+++ crates/engraver/tests/it/accidentals.rs
@@ -1,3 +1,5 @@
+use color_eyre::eyre::Result;
+
 #[test]
 fn no_accidental_when_no_alteration_in_key_with_no_sharps() {
     assert_staff_snapshot!("f |");
@@ -36,4 +38,11 @@ fn accidental_when_alteration_repeated_over_barline() {
 #[test]
 fn accidental_canceled_in_next_bar() {
     assert_staff_snapshot!("f# | f |");
+}
+
+#[test]
+fn key_signature_alteration() -> Result<()> {
+    assert_staff_snapshot!("c d# e f | c d# e# f ||");
+
+    Ok(())
 }
